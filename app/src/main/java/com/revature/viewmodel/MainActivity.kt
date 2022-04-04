53af81bf572d87039e0116f4e433e7f0c4bb3d8b
RyanKCox <ryankcox90@gmail.com>
2022-04-04 10:51:37 -0500

    Initial commit

---
+++ app/src/main/java/com/revature/viewmodel/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.revature.viewmodel
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.revature.viewmodel.ui.theme.ViewModelTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ViewModelTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    
+                }
+            }
+        }
+    }
+}
